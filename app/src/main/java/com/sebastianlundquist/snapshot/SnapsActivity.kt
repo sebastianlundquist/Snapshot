e46fd12880550afbaca4d506d1e8f5de9960b84e
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-10-05 15:04:34 +0200

    Modify snaps activity to receive snaps in real time

--- app/src/main/java/com/sebastianlundquist/snapshot/SnapsActivity.kt
+++ app/src/main/java/com/sebastianlundquist/snapshot/SnapsActivity.kt
@@ -5,16 +5,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
 class SnapsActivity : AppCompatActivity() {
 
     val mAuth = FirebaseAuth.getInstance()
+    var snapsListView : ListView? = null
+    var emails : ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snaps)
-        setTitle(R.string.users_title)
+        setTitle(R.string.feed_title)
+
+        snapsListView = findViewById(R.id.snapsListView)
+        val adapter = ArrayAdapter(this, R.layout.custom_textview, emails)
+        snapsListView?.adapter = adapter
+
+        FirebaseDatabase.getInstance().reference.child("users").child(mAuth.currentUser!!.uid).child("snaps").addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                emails.add(p0.child("from").value as String)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(p0: DatabaseError) {}
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {}
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {}
+            override fun onChildRemoved(p0: DataSnapshot) {}
+
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
