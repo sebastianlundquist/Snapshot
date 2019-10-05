0ecff79e4d4660313fa0189e79159614cc55a109
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-10-05 13:21:23 +0200

    Create a bitmap and upload to Firestore when next button is clicked

--- app/src/main/java/com/sebastianlundquist/snapshot/CreateSnapActivity.kt
+++ app/src/main/java/com/sebastianlundquist/snapshot/CreateSnapActivity.kt
@@ -11,11 +11,18 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class CreateSnapActivity : AppCompatActivity() {
 
     var createSnapImageView: ImageView? = null
     var messageEditText: EditText? = null
+    val imageName = UUID.randomUUID().toString() + ".jpg"
 
     fun chooseImageClicked(view: View) {
         if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -27,7 +34,29 @@ class CreateSnapActivity : AppCompatActivity() {
     }
 
     fun nextClicked(view: View) {
+        createSnapImageView?.isDrawingCacheEnabled = true
+        createSnapImageView?.buildDrawingCache()
+        val bitmap = (createSnapImageView?.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
 
+        val uploadTask = FirebaseStorage.getInstance().reference.child("images").child(imageName).putBytes(data)
+        uploadTask.addOnFailureListener {
+            Toast.makeText(this, "Upload failed.", Toast.LENGTH_SHORT).show()
+        }.addOnSuccessListener { taskSnapshot ->
+            var url: String?
+            val downloadUrl = taskSnapshot.metadata?.reference?.downloadUrl
+            downloadUrl?.addOnCompleteListener { _ ->
+                if (uploadTask.isSuccessful) {
+                    url = downloadUrl.result.toString()
+                    Log.i("Url", url)
+                }
+                else {
+                    Toast.makeText(this, uploadTask.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     fun getPhoto() {
@@ -60,7 +89,7 @@ class CreateSnapActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         val selectedImage = data!!.data
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
             try {
                 var bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
                 bitmap = cropToSquare(bitmap)
